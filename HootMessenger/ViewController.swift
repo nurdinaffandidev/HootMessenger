d87d119db2728410b7c3bb3ab5aac8648b43a604
nurdinaffandidev <nurdinaffandidev@gmail.com>
2024-10-23 21:24:48 +0800

    Initial Commit

---
+++ HootMessenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HootMessenger
+//
+//  Created by nurdin affandi on 23/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
