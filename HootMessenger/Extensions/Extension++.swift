b52cacfa904164c91d449a962f5175238fcea7ad
nurdinaffandidev <nurdinaffandidev@gmail.com>
2024-10-26 18:41:35 +0800

    feat[Extensions]: create extension file

---
+++ HootMessenger/Extensions/Extension++.swift
@@ -0,0 +1,45 @@
+//
+//  Extension++.swift
+//  HootMessenger
+//
+//  Created by nurdin affandi on 26/10/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+
+    public var width: CGFloat {
+        return frame.size.width
+    }
+
+    public var height: CGFloat {
+        return frame.size.height
+    }
+
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+
+    public var bottom: CGFloat {
+        return frame.size.height + frame.origin.y
+    }
+
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+
+    public var right: CGFloat {
+        return frame.size.width + frame.origin.x
+    }
+    
+    public var topPadding: CGFloat {
+        return window?.safeAreaInsets.top ?? 0
+    }
+    
+    public var bottomPadding: CGFloat {
+        return window?.safeAreaInsets.bottom ?? 0
+    }
+
+}
